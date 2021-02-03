897ab1f4c5dcac046b12ff7c91b179045305632f
jiniya22 <yyj22@bankq.co.kr>
2021-02-03 14:40:38 +0900

    sshtunnel을 이용한 private mysql server connection 테스트

---
+++ 1_pymysql/02_sshtunnel.py
@@ -0,0 +1,20 @@
+import pymysql
+from sshtunnel import SSHTunnelForwarder
+
+
+if __name__ == '__main__':
+    with SSHTunnelForwarder(('jiniworld.me'),
+                            ssh_username='jini', ssh_pkey='C:/Users/jini/.ssh/apple.pem', ssh_port=22222,
+                            remote_bind_address=('127.0.0.1', 3306)) as tunnel:
+        with pymysql.connect(host='127.0.0.1', user='test2', password='test2', port=tunnel.local_bind_port,
+                             db='test_db', charset="utf8", cursorclass=pymysql.cursors.DictCursor) as conn:
+            with conn.cursor() as cur:
+                sql = '''
+                SELECT * from users
+                '''
+                cur.execute(sql)
+                print(sql)
+                results = cur.fetchall()
+                for result in results:
+                    print(result)
+    print('\nfinish!\n', '-----' * 20)
